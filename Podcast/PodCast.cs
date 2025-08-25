608a26a0a5f8c023d4fe6d51d105910f9e4c9a69
Kaio Melugo <kaiomelugo8876@gmail.com>
2025-08-24 22:26:41 -0300

    Projeto utilizando POO

---
+++ Podcast/PodCast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Podcast
+{
+    private List<Episidio> episodios = new List<Episidio>();
+    public Podcast(string host, string nome)
+    {
+        Nome = nome;
+        Host = host;
+    }
+
+    public string Host { get; set; }
+    public string Nome { get; set; }
+    public int TotalEpisodios => episodios.Count;
+
+    
+
+    public void AdicionarEpisodio(Episidio episodio)
+    {
+        episodios.Add(episodio);
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Podcast >| {Nome} |< Apresentado por [{Host}]\n");
+        foreach (Episidio ep in episodios)
+        {
+            Console.WriteLine(ep.Resumo);
+        }
+
+        Console.WriteLine($"\n\n Total de Episodios: {TotalEpisodios}.\n\n");
+    }
+}
---
+++ Podcast/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodCast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Episidio ep1 = new Episidio (1, "aula de ds1", 50);
+            ep1.AdicionarConvidados("Hélio Silva");
+            ep1.AdicionarConvidados("Kaio");
+
+            Episidio ep2 = new Episidio(2, "DS2", 35);
+            ep2.AdicionarConvidados("Nica");
+            ep2.AdicionarConvidados("Kaio");
+
+            Episidio ep3 = new Episidio(3, "DS3", 55);
+            ep3.AdicionarConvidados("Lazaro");
+            ep3.AdicionarConvidados("Kaio");
+
+       
+            Podcast podcast1 = new Podcast("Hélio Silva", "Etec Itapevi");
+            
+            podcast1.AdicionarEpisodio(ep1);
+            podcast1.AdicionarEpisodio(ep2);
+            podcast1.AdicionarEpisodio(ep3);
+            podcast1.ExibirDetalhes();
+
+            Podcast podcast2 = new Podcast("Hélio Silva", "Etec Itapevi");
+
+            podcast2.AdicionarEpisodio(ep1);
+            podcast2.AdicionarEpisodio(ep2);
+            podcast2.AdicionarEpisodio(ep3);
+            podcast2.ExibirDetalhes();
+            Console.ReadKey();
+        }
+    }
+}
